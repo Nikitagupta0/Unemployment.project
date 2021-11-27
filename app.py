c1a3ef3496df2cd4d642057253b09eb63633205c
Nikitagupta0 <nikitagupta4246@gmail.com>
2021-11-27 17:07:08 +0530

    DONE

--- app.py
+++ app.py
@@ -37,42 +37,47 @@ choice = st.sidebar.selectbox("select an option",options)
 data = load_data()
 # st.write(data.columns.tolist())
 if choice == options[0]:
-    st.image("unemployment.jpg")
+    st.image("unemployment.jpg",caption='Unemployment in India', width=700)
     st.info('''One of the major social issues in India is unemployment.As the Indian labour laws are inflexible and restrictive, and its infrastructure is poor, which is actually the main reason for India’s unemployment situation, according to The Economist. 
     As of September 2018, according to the Indian Government, India had 31 million jobless people. The scenario of Assam, in the case of unemployment, is also worst. As per statistics made available by the state Skill Employment and Entrepreneurship department, the total numbers of registered employed in the state is 19,63,376; of them, 16,65,866 are educated or skilled ones and  2,97,510 unskilled ones. ''')
     st.write(data)
     
 elif choice == options[1]:
-    statelist = data.Region.unique().tolist()
+    statelist = data.States.unique().tolist()
     statelist = [str(v) for v in statelist]
     statelist.sort()
     statelist.pop()
     # st.write(statelist)
     state = st.selectbox("select a state",statelist)
     try:
+        color=st.sidebar.color_picker("select graph color")
         fig,ax = plt.subplots()
-        data_state = data[data['Region']==state].set_index('Date')['Estimated Unemployment Rate']
+        data_state = data[data['States']==state].set_index('Date')['Estimated Unemployment Rate']
         state_data = data_state.resample('M').sum()
-        fig = px.line(state_data, y= "Estimated Unemployment Rate")
+        fig = px.line(state_data, y= "Estimated Unemployment Rate",)
         st.plotly_chart(fig)
+        st.info(''' 
+        ''')
     except Exception as e:
         st.error(f"Please ask the admin {e}")
 
 elif choice == options[2]:
+    color=st.sidebar.color_picker("select graph color")
     fig,ax = plt.subplots()
     op = st.selectbox("select a category",["Estimated Unemployment Rate","Estimated Employed","Estimated Labour Participation Rate"])
-    data.groupby(['States'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='States',figsize=(15,10),ax=ax)
+    data.groupby(['States'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='States',figsize=(15,10),ax=ax,color=color)
     st.pyplot(fig)
 
 elif choice == options[3]:
+    color=st.sidebar.color_picker("select graph color")
     fig,ax = plt.subplots()
-    
     plt.title("Indian Unemployment")
     fig = px.histogram(data,x="Estimated Employed")
     st.plotly_chart(fig)
 
 elif choice == options[4]:
+    color=st.sidebar.color_picker("select graph color")
     fig,ax = plt.subplots()
     op = st.selectbox("select a category",["Estimated Unemployment Rate","Estimated Employed","Estimated Labour Participation Rate"])
-    data.groupby(['Region'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='Region',figsize=(15,10),ax=ax)
+    data.groupby(['Region'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='Region',figsize=(15,10),ax=ax,color=color)
     st.pyplot(fig)
