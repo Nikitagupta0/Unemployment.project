5c2e0009d99accee6ea90d49be46d80fdcb9a0a5
Nikitagupta0 <nikitagupta4246@gmail.com>
2021-11-27 16:13:22 +0530

    done

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.dates import DateFormatter
 import numpy as np
+import plotly.express as px
 
 
 plt.style.use('seaborn')
@@ -14,6 +15,11 @@ def load_data():
     data= read_csv("Unemployment in India.csv",parse_dates=[' Date'],dayfirst=True)
     clean_cols = [col.strip() for col in data.columns.tolist()]
     data.columns = clean_cols
+    data.columns = ["States","Date","Frequency",
+                "Estimated Unemployment Rate",
+                "Estimated Employed",
+                "Estimated Labour Participation Rate",
+                "Region"]
     return data
 
 st.title("Unemployment in India")
@@ -22,9 +28,9 @@ st.sidebar.header("Project Options")
 options = [
             "Overview",
             "Timeline analysis",
-            "Estimated Unemployment Rate",
+            "Unemployment Rate",
             "Estimated Employed",
-            "Region"]
+            "Region wise"]
 
 choice = st.sidebar.selectbox("select an option",options)
 
@@ -32,58 +38,41 @@ data = load_data()
 # st.write(data.columns.tolist())
 if choice == options[0]:
     st.image("unemployment.jpg")
-    st.info('''One of the major social issues in India is unemployment.As the Indian labour laws are inflexible and restrictive, and its infrastructure is poor, which is actually the main reason for India’s unemployment situation, according to The Economist. As of September 2018, according to the Indian Government, India had 31 million jobless people. The scenario of Assam, in the case of unemployment, is also worst. As per statistics made available by the state Skill Employment and Entrepreneurship department, the total numbers of registered employed in the state is 19,63,376; of them, 16,65,866 are educated or skilled ones and  2,97,510 unskilled ones. ''')
-    fig,ax = plt.subplots()
-    data.hist(edgecolor="black",linewidth = 1.2 , figsize=(10,10),ax=ax)
-    st.pyplot(fig)
+    st.info('''One of the major social issues in India is unemployment.As the Indian labour laws are inflexible and restrictive, and its infrastructure is poor, which is actually the main reason for India’s unemployment situation, according to The Economist. 
+    As of September 2018, according to the Indian Government, India had 31 million jobless people. The scenario of Assam, in the case of unemployment, is also worst. As per statistics made available by the state Skill Employment and Entrepreneurship department, the total numbers of registered employed in the state is 19,63,376; of them, 16,65,866 are educated or skilled ones and  2,97,510 unskilled ones. ''')
+    st.write(data)
     
 elif choice == options[1]:
-    color=st.sidebar.color_picker("select graph color")
-    state = st.sidebar.radio("select a state",data.Region.unique().tolist())
+    statelist = data.Region.unique().tolist()
+    statelist = [str(v) for v in statelist]
+    statelist.sort()
+    statelist.pop()
+    # st.write(statelist)
+    state = st.selectbox("select a state",statelist)
     try:
         fig,ax = plt.subplots()
-        data_state = data[data['Region']==state].set_index('Date')['Estimated Unemployment Rate (%)']
-        data_state.resample('M').sum().plot(kind='line',style='o--',figsize=(15,5),ax=ax,color=color)
-        plt.title(f'UNEMPLOYMENT in {state}', fontsize = '11',color = 'c')
-        
-        st.pyplot(fig)
+        data_state = data[data['Region']==state].set_index('Date')['Estimated Unemployment Rate']
+        state_data = data_state.resample('M').sum()
+        fig = px.line(state_data, y= "Estimated Unemployment Rate")
+        st.plotly_chart(fig)
     except Exception as e:
         st.error(f"Please ask the admin {e}")
 
 elif choice == options[2]:
     fig,ax = plt.subplots()
-    data.groupby(['Region'])['Estimated Unemployment Rate'].sum().reset_index().sort_values(by='Estimated Unemployment Rate').tail(10).plot(kind='bar',x='Region',figsize=(15,10),ax=ax)
+    op = st.selectbox("select a category",["Estimated Unemployment Rate","Estimated Employed","Estimated Labour Participation Rate"])
+    data.groupby(['States'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='States',figsize=(15,10),ax=ax)
     st.pyplot(fig)
 
 elif choice == options[3]:
     fig,ax = plt.subplots()
-    data.columns = ["States","Date","Frequency",
-                "Estimated Unemployment Rate",
-                "Estimated Employed",
-                "Estimated Labour Participation Rate",
-                "Region"]
+    
     plt.title("Indian Unemployment")
-    sns.histplot(x="Estimated Employed",hue="Region",data=data,ax=ax)
-    st.pyplot(fig)
+    fig = px.histogram(data,x="Estimated Employed")
+    st.plotly_chart(fig)
 
 elif choice == options[4]:
     fig,ax = plt.subplots()
-    data.hist(edgecolor="black",linewidth = 1.2 , figsize=(10,10),ax=ax)
-    d = data.Region.value_counts().head(10)                 
-    plt.figure(figsize=(10,10))
-    x = list(d.index)
-    y = list(d.values)
-    x.reverse()
-    y.reverse()
-    plt.title("Estimated Unemployment in Regions")
-    plt.ylabel("Unemployment Count")
-    plt.xlabel("Regions")
-    plt.bar(x,y)
+    op = st.selectbox("select a category",["Estimated Unemployment Rate","Estimated Employed","Estimated Labour Participation Rate"])
+    data.groupby(['Region'])[op].sum().reset_index().sort_values(by=op).tail(10).plot(kind='bar',x='Region',figsize=(15,10),ax=ax)
     st.pyplot(fig)
-
-
-elif choice == options[5]:
-    fig,ax = plt.subplots()
-    data.groupby(['Region'])['Estimated Unemployment Rate'].sum().reset_index().sort_values(by='Estimated Unemployment Rate').tail(10).plot(kind='bar',x='Region',figsize=(15,10))
-    st.pyplot(fig)
-
